92e18c9dd9e81b33da91c90054ff8cd0831db0cb
lthomas32 <lthomas32@ewu.edu>
2023-05-17 08:18:32 -0700

    added the q statement for quit

--- Chapter1/Bank1_OneAccount.py
+++ Chapter1/Bank1_OneAccount.py
@@ -12,6 +12,7 @@ while True:
     print('Press d to make a deposit')
     print('Press w to make a withdrawal')
     print('Press s to show the account')
+    print('Press q to quit')
     print()
 
     action = (input('What do you want to do? ')).lower()
